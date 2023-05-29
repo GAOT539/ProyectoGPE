4a43a0e67177596164102f6e027df64827a2af52
GAOT28 <gabrielocampot@gmail.com>
2023-05-29 05:54:10 -0500

    Correcion

--- Forms/Form_CreateColumna.cs
+++ Forms/Form_CreateColumna.cs
@@ -1,12 +1,12 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProyectoSGBD_MySQL.Forms
