6097f15b073f78c05e7bece818d9e991737a3878
Paola Padilla <ppadilla3364@uta.edu.ec>
2023-07-01 10:05:27 -0500

    Form_Fragmentacion

---
+++ Forms/Form_Fragmentacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSGBD_MySQL
+{
+    public partial class Form_Fragmentacion : Form
+    {
+        public Form_Fragmentacion()
+        {
+            InitializeComponent();
+        }
+    }
+}
